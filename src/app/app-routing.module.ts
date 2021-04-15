58c9655012ef442f11519f24776d74d74bafa65c
Ricardo Ignacio Gutiérrez León <59661365+RicardoLeon15@users.noreply.github.com>
2021-04-15 02:53:13 -0500

    interfaz iniciar sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './componentes/home/home.component';
 import { UsoComponent } from './componentes/uso/uso.component';
 import { CuentaComponent } from './componentes/cuenta/cuenta.component';
 import { EntradaComponent } from './componentes/entrada/entrada.component';
+import { AutenticarseFormComponent } from './componentes/autenticarse-form/autenticarse-form.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'uso', component: UsoComponent},
   {path: 'entrada', component: EntradaComponent},
-  {path: 'cuenta', component: CuentaComponent}
+  {path: 'cuenta', component: CuentaComponent},
+  {path: 'autenticarse', component: AutenticarseFormComponent}
 ];
 
 @NgModule({
---
+++ src/app/componentes/autenticarse-form/autenticarse-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autenticarse-form',
+  templateUrl: './autenticarse-form.component.html',
+  styleUrls: ['./autenticarse-form.component.css']
+})
+export class AutenticarseFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
