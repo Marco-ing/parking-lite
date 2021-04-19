2c527dfb68623f82cb8b4bb6f116a8e23814f5de
Hugo Chiquito <alex.400@live.com.mx>
2021-04-18 22:05:38 -0500

    Registro
    Registro pero ahora con validaciones

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,14 +8,34 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegistroComponent implements OnInit {
 
-  //Formulario registrarse responsivo
-  registrarse: FormGroup = new FormGroup({
-    
-  })
-
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+  
 
+  //Formulario registrarse responsivo
+  registrarse: FormGroup = this.fb.group({
+    nombre          : ['',[Validators.required]],
+    apellidoPaterno : ['',Validators.required],
+    apellidoMaterno : ['',Validators.required],
+    correo          : ['',[Validators.required,Validators.email]],
+    password          : ['',[Validators.required]],
+  })
+
+  //Funcion para validar campos
+  campoEsValido(campo:string){
+    return this.registrarse.controls[campo].errors 
+    && this.registrarse.controls[campo].touched
+  }
+
+  //Guardar datos formulario
+  guardar(){
+    if(this.registrarse.invalid){
+      this.registrarse.markAllAsTouched();
+      return;
+    }
+    console.log(this.registrarse.value);
+    
+  }
 }
